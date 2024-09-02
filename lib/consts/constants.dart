30c54cba0fa8c808d037f48993a068c3d1764c6c
OracleMatrix <ehsanmohamadipoor@gmail.com>
2024-09-02 13:19:24 +0330

    Constant values

--- lib/consts/constants.dart
+++ lib/consts/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Constants {
   static Color primaryColor = const Color(0xff296e48);
-  static Color blackColor = Colors.black;
+  static Color blackColor = const Color(0xff737373);
 
   static String titleOne = "گیاهان را بهتر از قبل درک کن";
   static String descriptionOne =

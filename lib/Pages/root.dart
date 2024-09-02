ba55e1411d5178c15d08db041cd8edff86e794cc
OracleMatrix <ehsanmohamadipoor@gmail.com>
2024-09-02 13:20:03 +0330

    const app bar and bottom navigation options

--- lib/Pages/root.dart
+++ lib/Pages/root.dart
@@ -1,4 +1,12 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:plant_app/Pages/cart_page.dart';
 import 'package:flutter/material.dart';
+import 'package:plant_app/Pages/favorite_page.dart';
+import 'package:plant_app/Pages/home_page.dart';
+import 'package:plant_app/Pages/profile_page.dart';
+import 'package:plant_app/Pages/scan_page.dart';
+import 'package:plant_app/consts/constants.dart';
 
 class RootPage extends StatefulWidget {
   const RootPage({super.key});
@@ -8,11 +16,92 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
+  int bottomIndex = 0;
+  List<Widget> pages = const [
+    HomePage(),
+    FavoritPage(),
+    CartPage(),
+    ProfilePage(),
+  ];
+
+  List<IconData> iconsList = [
+    Icons.home,
+    Icons.favorite,
+    Icons.shopping_cart,
+    Icons.person,
+  ];
+
+  List<String> appBarTitles = [
+    "خانه",
+    "علاقه مندی ها",
+    "سبدخرید",
+    "پروفایل",
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("App bar"),
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 80,
+          elevation: 0,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          title: Text(
+            appBarTitles[bottomIndex],
+            style: TextStyle(
+              fontFamily: "Lalezar",
+              color: Constants.blackColor,
+              fontSize: 25,
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Icon(
+                Icons.notifications,
+                size: 30,
+                color: Constants.blackColor,
+              ),
+            ),
+          ],
+        ),
+        body: IndexedStack(
+          index: bottomIndex,
+          children: pages,
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              PageTransition(
+                child: const ScanPage(),
+                type: PageTransitionType.bottomToTop,
+              ),
+            );
+          },
+          shape: const CircleBorder(),
+          backgroundColor: Constants.primaryColor,
+          child: Image.asset(
+            "assets/images/code-scan-two.png",
+            height: 30,
+          ),
+        ),
+        bottomNavigationBar: AnimatedBottomNavigationBar(
+          splashColor: Constants.primaryColor,
+          activeColor: Constants.primaryColor,
+          inactiveColor: Constants.blackColor.withOpacity(0.5),
+          gapLocation: GapLocation.center,
+          notchSmoothness: NotchSmoothness.softEdge,
+          icons: iconsList,
+          activeIndex: bottomIndex,
+          onTap: (p0) {
+            setState(() {
+              bottomIndex = p0;
+            });
+          },
+        ),
       ),
     );
   }

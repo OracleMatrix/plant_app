48221d9fcf9dcfaa1c5c36de734047c2e1e336e0
OracleMatrix <ehsanmohamadipoor@gmail.com>
2024-09-05 22:38:51 +0330

    Favorited items screen

--- lib/Pages/favorite_page.dart
+++ lib/Pages/favorite_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/Widgets/plant_widget.dart';
+import 'package:plant_app/consts/constants.dart';
+import 'package:plant_app/models/plant.dart';
 
 class FavoritPage extends StatefulWidget {
-  const FavoritPage({super.key});
+  final List<Plant> favortiedPlants;
+  const FavoritPage({super.key, required this.favortiedPlants});
 
   @override
   State<FavoritPage> createState() => _FavoritPageState();
@@ -10,10 +14,36 @@ class FavoritPage extends StatefulWidget {
 class _FavoritPageState extends State<FavoritPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(""),
-      ),
+    return Scaffold(
+      body: widget.favortiedPlants.isEmpty
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/images/favorited.png",
+                    height: 100,
+                  ),
+                  Text(
+                    "ظاهرا به هیچی علاقه نداشتی!",
+                    style: TextStyle(
+                      color: Constants.primaryColor,
+                      fontFamily: "iranSans",
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          : ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: widget.favortiedPlants.length,
+              itemBuilder: (context, index) {
+                return NewPlantWidget(
+                    plantsList: widget.favortiedPlants, index: index);
+              },
+            ),
     );
   }
 }

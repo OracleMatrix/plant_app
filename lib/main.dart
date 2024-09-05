c3fe76f3d28e5307bc393e640bcb8519865b421c
OracleMatrix <ehsanmohamadipoor@gmail.com>
2024-09-06 00:31:52 +0330

    main files

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Directionality(
+      /*
+      for persian language
+       */
       textDirection: TextDirection.rtl,
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
